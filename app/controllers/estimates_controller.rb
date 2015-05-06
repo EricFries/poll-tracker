b41ef955da0cc27aa2c7f63c45b2e5d3fa96f3bc
EricFries <ericfries@gmail.com>
2015-05-06 11:53:33 -0400

    move js from partials to load.js

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,8 +1,5 @@
 class EstimatesController < ApplicationController
-  before_action :set_estimate, only: [:show, :edit, :update, :destroy]
 
-  # GET /estimates
-  # GET /estimates.json
   def index
     @custom_matchup = Estimate.custom_matchup(["Sanders", "Clinton", "Biden"])
 
@@ -28,56 +25,11 @@ class EstimatesController < ApplicationController
 
     @gop_candidates = @gop_estimate.collect {|can_hash| can_hash[:choice]}
     @gop_poll_numbers = @gop_estimate.collect {|can_hash| can_hash[:value]}
-  end
-
-  # POST /estimates
-  # POST /estimates.json
-  def create
-    @estimate = Estimate.new(estimate_params)
-
+    
     respond_to do |format|
-      if @estimate.save
-        format.html { redirect_to @estimate, notice: 'Estimate was successfully created.' }
-        format.json { render :show, status: :created, location: @estimate }
-      else
-        format.html { render :new }
-        format.json { render json: @estimate.errors, status: :unprocessable_entity }
-      end
+      format.html
+      format.js
     end
   end
 
-  # PATCH/PUT /estimates/1
-  # PATCH/PUT /estimates/1.json
-  def update
-    respond_to do |format|
-      if @estimate.update(estimate_params)
-        format.html { redirect_to @estimate, notice: 'Estimate was successfully updated.' }
-        format.json { render :show, status: :ok, location: @estimate }
-      else
-        format.html { render :edit }
-        format.json { render json: @estimate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /estimates/1
-  # DELETE /estimates/1.json
-  def destroy
-    @estimate.destroy
-    respond_to do |format|
-      format.html { redirect_to estimates_url, notice: 'Estimate was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_estimate
-      @estimate = Estimate.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def estimate_params
-      params[:estimate]
-    end
 end
